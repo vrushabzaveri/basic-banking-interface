0dd051f9ac0867845223fd5f4f846406331e72c8
Vrushab Zaveri <114823340+vrushabzaveri@users.noreply.github.com>
2023-05-29 17:02:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- navbar starts here  -->
+    <nav class="navbar">
+        <a href="index.php">Nimbuspay Bank</a>
+    </nav>
+    <!-- navbar ends here  -->
+
+    <div class="container">
+        <div class="content">
+            <h1>Welcome to Nimbuspay Bank</h1>
+            <p>Your Transactions Made Easy</p>
+            <br>
+            <a class="transfer" href="customers.php">Customers</a>
+        </div>
+
+        <div class="image">
+            <img src="money-transfer.svg" alt="money-transfer">
+        </div>
+
+    </div>
+</body>
+
+</html>
